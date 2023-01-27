40e9c525db24380e5750ddf70bf54dfb47e84bc6
basemelazab <basem.elazab2014@gmail.com>
2023-01-27 07:48:35 +0200

    Add project files.

---
+++ CRUDApplication/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
